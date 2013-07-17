f6a376eb2607e284cd24d0f178971144edec3f82
Juha-Matti Mäkinen <juhamattimakinen.con@gmail.com>
2013-07-17 15:37:40 +0300

    Altering test
    Integrating the stuff further into testframework usable content.
    Problem rose with the subprocess' assert method.

--- tests/testing1FolderDivisionTo1Folder.py
+++ tests/testing1FolderDivisionTo1Folder.py
@@ -9,9 +9,10 @@ class TestDivision(unittest.TestCase):
 
   def setUp(self):
     self.argumentsForProgram=['../codes','divide.py', 'divideTestFolder', 'output1']
+
   def testGivingArguments(self):
-    assert subprocess.call(self.argumentsForProgram)
-  def testAcceptance(self):
+    self.assert subprocess.call(self.argumentsForProgram)
     self.assertEquals(os.listdir(output1), ['testFile1.txt', 'testFile2.txt'])
+
 if __name__=='__main__':
   unittest.main()
