4633bc1fe57a17ec7c5d4b58624d5b31dff8415c
Juha-Matti Mäkinen <juhamattimakinen.con@gmail.com>
2013-07-19 16:02:14 +0300

    Creation of folder, modification to divide.py
    Worked over the code and added a existing folder for the program to
    divide once ready.

--- divide/divide.py
+++ divide/divide.py
@@ -3,11 +3,12 @@ import os, sys, shutil, errno
 commands=sys.argv[1:]
 def dividingFolder(commands):
   os.mkdir(commands[1])
+  shutil.copytree(commands[0],commands[1])
   
 
 
 def errorHandle():
-  if len(commands)=<1:
+  if len(commands)<=1:
     print 'usage: /divide.py sourcefolder outputfolder'
     sys.exit(1)
 
