86db19b86499b543c6e9c02f1415ea2b898f25e1
Juha-Matti Mäkinen <juhamattimakinen.con@gmail.com>
2013-07-29 14:04:38 +0300

    New file made
    This should correct the line14problem in original code, by making it
    more simplistic, breaking down the error inducing subprocess into
    various other parts.

---
+++ tests/line14ProblemSolveTest.py
@@ -0,0 +1,22 @@
+import sys
+import os
+import unittest
+import yamf
+import subprocess
+from yamf import Mock, MockModule, MockArray
+
+programDir=r"D:\users\Karethas_2\Documents\GitHub\divide\divide"
+assert os.path.isdir(programDir)
+os.chdir(programDir)
+output1='D:\users\Karethas_2\Documents\GitHub\divide\divide\output1'
+class TestDivision(unittest.TestCase):
+
+  def setUp(self):
+    self.argumentsForProgram='divide1.py divideTestFolder output1'
+
+  def testGivingArguments(self):
+    os.system(self.argumentsForProgram)
+    self.assertEquals(os.listdir(output1), ['testFile1.txt', 'testFile2.txt'])
+
+if __name__=='__main__':
+  unittest.main()
