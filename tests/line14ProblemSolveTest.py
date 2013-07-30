374aeb1964968afca8c2c5b095c44076acd1a7fb
Juha-Matti Mäkinen <juhamattimakinen.con@gmail.com>
2013-07-30 21:07:04 +0300

    second test ready and failed

--- tests/line14ProblemSolveTest.py
+++ tests/line14ProblemSolveTest.py
@@ -8,15 +8,22 @@ from yamf import Mock, MockModule, MockArray
 programDir=r"D:\users\Karethas_2\Documents\GitHub\divide\divide"
 assert os.path.isdir(programDir)
 os.chdir(programDir)
-output1='D:\users\Karethas_2\Documents\GitHub\divide\divide\output1'
+output1='..\divide\output1'
+output2='..\divide\output2'
+output3='..\divide\output3'
+
 class TestDivision(unittest.TestCase):
 
   def setUp(self):
-    self.argumentsForProgram='divide.py divideTestFolder output1'
+    self.argumentsForProgram='divide.py divideTestFolder output1 output2 output3'
 
   def testGivingArguments(self):
     os.system(self.argumentsForProgram)
-    self.assertEquals(os.listdir(output1), ['testFile1.txt', 'testFile2.txt'])
+    self.assertEquals(os.listdir(output1), ['testFile1.txt'])
+    self.assertEquals(os.listdir(output2), ['testFile2.txt'])
+    self.assertEquals(os.listdir(output3), ['testFile3.txt'])
 
+	
+	
 if __name__=='__main__':
   unittest.main()
--- tests/testing1FolderDivisionTo1Folder.py
+++ tests/testing1FolderDivisionTo1Folder.py
@@ -5,14 +5,25 @@ import yamf
 import subprocess
 from yamf import Mock, MockModule, MockArray
 
+programDir=r"D:\users\Karethas_2\Documents\GitHub\divide\divide"
+assert os.path.isdir(programDir)
+os.chdir(programDir)
+output1='..\divide\output1'
+output2='..\divide\output2'
+output3='..\divide\output3'
+
 class TestDivision(unittest.TestCase):
 
   def setUp(self):
-    self.argumentsForProgram=['../divide','divide.py divideTestFolder output1']
+    self.argumentsForProgram='divide.py divideTestFolder output1 output2 output3'
 
   def testGivingArguments(self):
-    self.assertTrue(subprocess.call(self.argumentsForProgram))
-    self.assertEquals(os.listdir(output1), ['testFile1.txt', 'testFile2.txt'])
+    os.system(self.argumentsForProgram)
+    self.assertEquals(os.listdir(output1), ['testFile1.txt'])
+    self.assertEquals(os.listdir(output2), ['testFile2.txt'])
+    self.assertEquals(os.listdir(output3), ['testFile3.txt'])
 
+	
+	
 if __name__=='__main__':
   unittest.main()
