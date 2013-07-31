0fc2ee478728d3c2fb955f106ec8379183388d11
Juha-Matti Mäkinen <juhamattimakinen.con@gmail.com>
2013-07-31 15:24:46 +0300

    Second test pass

--- divide/divide.py
+++ divide/divide.py
@@ -1,4 +1,13 @@
 import os, sys, shutil, errno
 
 commands=sys.argv[1:]
-shutil.copytree(commands[0],commands[1])
+commandholder=[]
+
+for command in commands:
+  if command == commands[0]:
+    print command
+  elif command > commands[0]:
+	commandholder.append(command)
+	print commandholder
+	shutil.copytree(commands[0],''.join(commandholder))
+	commandholder.remove(command)
