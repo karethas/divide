8061222aae626ceb37f8796d6a8268a1e3570a5d
Juha-Matti Mäkinen <juhamattimakinen.con@gmail.com>
2013-08-02 15:55:23 +0300

    Minor mods to divide.py, addition of DividePlan.rtf

--- divide/divide.py
+++ divide/divide.py
@@ -2,13 +2,17 @@ import os, sys, shutil, errno
 
 commands=sys.argv[1:]
 commandholder=[]
-directoryContent=os.listdir(commands[0])
+DirectoryContent=os.listdir(commands[0])
 
 for command in commands:
   if command == commands[0]:
-    print command
+    print "Division in progress."
   elif command > commands[0]:
 	commandholder.append(command)
 	os.mkdir(''.join(commandholder))
-	
-	commandholder.remove(command)
+  for partFile in DirectoryContent:
+    fullFile = os.path.join(commands[0], partFile)
+    if (os.path.isfile(fullFile)):
+      shutil.copy(fileName, ''.join(commandholder))
+    commandholder.remove(command)
+	print "Division complete."
