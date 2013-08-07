315fbb0e305def79291b62f3f18e022c3ccb8479
Juha-Matti Mäkinen <juhamattimakinen.con@gmail.com>
2013-08-07 15:01:41 +0300

    nearly done with divide py

--- divide/divide.py
+++ divide/divide.py
@@ -1,20 +1,39 @@
 import os, sys, shutil, errno
 
 commands=sys.argv[1:]
-commandholder=[]
+commandHolder=[]
+folderName=[]
 directoryContent=os.listdir(commands[0])
+contentSize=0
 lengthOfArgumentsAfterArg0=[]
+
+for content in directoryContent:
+    contentSize+=os.path.getsize(commands[0]+'/'+content)
+
 
 for command in commands:
   if command == commands[0]:
     print "Division in progress."
   elif command > commands[0]:
-	commandholder.append(command)
-	os.mkdir(''.join(commandholder))
+	commandHolder.append(command)
+	os.mkdir(''.join(commandHolder))
 	lengthOfArgumentsAfterArg0.append(command)
-	commandholder.remove(command)
+	commandHolder.remove(command)
+
+
+for command in commands:
+  folderName.append(command)  
+  fileIterator=0
+  folderIterator=0
+  if os.path.getsize(''.join(folderName[folderIterator]))<os.path.getsize(commands[0])/len(lengthOfArgumentsAfterArg0):
+    shutil.copy(commands[0]+'/'+directoryContent[fileIterator],''.join(folderName[folderIterator]))
+    fileIterator+=1
+  else:
+	folderIterator+=1
+	
+print contentSize/len(lengthOfArgumentsAfterArg0)
 
 print directoryContent
 print len(lengthOfArgumentsAfterArg0)
-print os.path.getsize(commands[0])
-print os.path.getsize(commands[0])/len(lengthOfArgumentsAfterArg0)
+print os.path.getsize(commands[0]+'/')
+
