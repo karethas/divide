fe4e308298f5235eb59e0d2770ca8dfb21a2e8da
Juha-Matti Mäkinen <juhamattimakinen.con@gmail.com>
2013-08-09 15:18:41 +0300

    more revisions to division.py
    Iterations are needed widely for this to run without flaw.
    The names that come one by one and are removed after use keep the
    foldernames from getting ridiculously long.
    Without iteration on the create output folders function it made the
    outputs look like:
    output1
    output1output2
    output1output2output3
    I am generating a function to get output folder's content sizes so they
    can be used in the last function, which is dividefilestofolders, which
    needs comparison criteria, this makes it easier to compare the
    sourcefolder content to a single output folder using rotation over the
    output folders.

--- divide/divide.py
+++ divide/divide.py
@@ -1,25 +1,41 @@
 import os, sys, shutil, errno
 
-commands=sys.argv[1:]
-outputFolders=sys.argv[:1]
-folderName=[]
-directoryContent=os.listdir(commands[0])
-contentSize=0
+sourceFolder=sys.argv[1]
+outputFolders=sys.argv[2:]
+sourceContent=os.listdir(sourceFolder)
+outputContent=os.listdir(outputFolders)
 
-def getSourceFolderSize():
-  for content in directoryContent:
-    contentSize+=os.path.getsize(commands[0]+'/'+content)
+print ''.join(outputFolders)
+def createOutputFolders(outputFolders):
+  rotator=0
+  yksinimikerrallaan=[]
+  while len(outputFolders)>rotator:
+    yksinimikerrallaan.append(outputFolders[rotator])
+    os.mkdir(''.join(yksinimikerrallaan)) 
+    yksinimikerrallaan.remove(outputFolders[rotator])
+    rotator+=1
+  
+def getSourceFolderContentSize():
+  contentSize=0
+  for content in sourceContent:
+    contentSize+=os.path.getsize(sourceFolder+'/'+content)
+  return contentSize
 
-def createOutputFolders():
-	os.mkdir(''.join(outputFolders))
-
-def divideFilesToFolders():
-  for command in commands:
-    folderName.append(command)  
-    fileIterator=0
-    folderIterator=0
-    if os.path.getsize(''.join(folderName[folderIterator]))<os.path.getsize(commands[0])/len(lengthOfArgumentsAfterArg0):
-      shutil.copy(commands[0]+'/'+directoryContent[fileIterator],''.join(folderName[folderIterator]))
-      fileIterator+=1
-    else:
-	  folderIterator+=1
+def getOutputFolderContentSize():
+  contentSize=0
+  for content in outputContent:
+    contentSize+=os.path.getsize(outputContent+'/'+content)
+  
+def divideFilesToFolders(sourceFolder, getSourceFolderContentSize):
+  fileIterator=0
+  folderIterator=0
+  sourceFolderContentSize=getSourceFolderContentSize()
+  while os.path.getsize(''.join(outputFolders[folderIterator]))<sourceFolderContentSize/len(outputFolders):
+    shutil.copy(sourceFolder+'/'+sourceContent[fileIterator],''.join(outputFolders[folderIterator]))
+    fileIterator+=1
+  else:
+	folderIterator+=1
+	
+createOutputFolders(outputFolders)
+getSourceFolderContentSize()
+divideFilesToFolders(sourceFolder, getSourceFolderContentSize)
