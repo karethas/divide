1ac2961776e0388c8dc2429954ac941680140494
Juha-Matti Mäkinen <juhamattimakinen.con@gmail.com>
2013-08-13 15:53:19 +0300

    Revisions, removed useless code so new ones can
    be added for better, faster functionality.

--- divide/divide.py
+++ divide/divide.py
@@ -2,46 +2,39 @@ import os, sys, shutil, errno
 
 sourceFolder=sys.argv[1]
 outputFolders=sys.argv[2:]
-sourceContent=os.listdir(sourceFolder)
+sourceFolderContent=os.listdir(sourceFolder)
 
 def createOutputFolders(outputFolders):
-  rotator=0
-  yksinimikerrallaan=[]
-  while len(outputFolders)>rotator:
-    yksinimikerrallaan.append(outputFolders[rotator])
-    os.mkdir(''.join(yksinimikerrallaan)) 
-    yksinimikerrallaan.remove(outputFolders[rotator])
-    rotator+=1
-  return 
-createOutputFolders(outputFolders)
+  for outputFolder in outputFolders:
+    os.mkdir(''.join(outputFolder)) 
+
   
 def getSourceFolderContentSize():
   contentSize=0
-  for content in sourceContent:
-    contentSize+=os.path.getsize(sourceFolder+'/'+content)
+  for content in sourceFolderContent:
+    contentSize+=os.path.getsize(os.path.join) IMPLEMENT
   return contentSize
   
 def getOutputFolderContentSize():
   outputSize=0
-  outputContent=os.listdir(outputFolders)
-  container=[]
-  for content in outputFolders:
-        container.append(content)
-	outputSize+=os.path.getsize(container+'/'+content)
-	container.remove(content)	  
+	outputSize+=os.path.getsize(os.path.join) IMPLEMENT	  
   return outputSize
 
-def divideFilesToFolders(sourceFolder, outputFolders, sourceContent, getSourceFolderContentSize, getOutputFolderContentSize):
+def divideFilesToFolders(sourceFolder, outputFolders, sourceFolderContent, getSourceFolderContentSize, getOutputFolderContentSize):
   fileIterator=0
   folderIterator=0
   sourceFolderContentSize=getSourceFolderContentSize()
   outputFolderContentSize=getOutputFolderContentSize()
   while folderIterator<len(outputFolders):
-        shutil.copy(sourceFolder+'/'+sourceContent[fileIterator],''.join(outputFolders[folderIterator]))
+        shutil.copy(sourceFolder+'/'+sourceFolderContent[fileIterator],''.join(outputFolders[folderIterator]))
 	fileIterator+=1
 	folderIterator+=1
 	print outputFolderContentSize
   if outputFolderContentSize>=sourceFolderContentSize/len(outputFolders):
 	  print 'life is perfect'  
+	 
 
-divideFilesToFolders(sourceFolder, outputFolders, sourceContent, getSourceFolderContentSize, getOutputFolderContentSize)
+createOutputFolders(outputFolders)
+getSourceFolderContentSize()
+getOutputFolderContentSize()
+divideFilesToFolders(sourceFolder, outputFolders, sourceFolderContent, getSourceFolderContentSize, getOutputFolderContentSize)
